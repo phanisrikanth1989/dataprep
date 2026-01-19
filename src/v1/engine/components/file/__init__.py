a56122cb976bcbb7fb8dfa76f14c73d25a12f774
phanisrikanth1989 <phani.srikanth1989@icloud.com>
2026-01-19 23:44:05 +0000

    feat: Add FileOutputDelimited component for writing delimited files and implement custom exception hierarchy

--- src/v1/engine/components/file/__init__.py
+++ src/v1/engine/components/file/__init__.py
@@ -3,8 +3,10 @@ File components - Input, Output, Delete operations
 """
 from .file_input_delimited import FileInputDelimited
 from .file_touch import FileTouch
+from .file_output_delimited import FileOutputDelimited
 
 __all__ = [
     "FileInputDelimited",
     "FileTouch",
+    "FileOutputDelimited",
 ]
